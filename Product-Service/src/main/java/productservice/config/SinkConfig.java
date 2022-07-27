77fc034670d291972cf12e59d1250550a323dfe4
Lenovo <Lenovo@DESKTOP-TAHOKJB>
2022-07-27 12:31:25 +0530

    Product-Service Project Commit

---
+++ Product-Service/src/main/java/productservice/config/SinkConfig.java
@@ -0,0 +1,23 @@
+package productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import productservice.dto.ProductDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@Configuration
+public class SinkConfig {
+
+    @Bean
+    public Sinks.Many<ProductDto> sink(){
+        return Sinks.many().replay().limit(1);
+    }
+
+    @Bean
+    public Flux<ProductDto> productBroadcast(Sinks.Many<ProductDto> sink){
+        return sink.asFlux();
+    }
+
+}
